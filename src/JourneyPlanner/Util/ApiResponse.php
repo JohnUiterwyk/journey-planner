cab6379c79538a4c6d32b1af4699d8126e1cf7b4
John Uiterwyk <jnuiterwyk@gmail.com>
2016-02-17 20:47:07 +1100

    add apiresponse helper class

---
+++ src/JourneyPlanner/Util/ApiResponse.php
@@ -0,0 +1,45 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: johnuiterwyk
+ * Date: 2/17/16
+ * Time: 1:41 PM
+ */
+
+namespace JourneyPlanner\Util;
+
+
+class ApiResponse
+{
+    private $status;
+    private $data;
+    public function setStatusSuccess()
+    {
+        $this->status = "success";
+    }
+    public function setStatusFail()
+    {
+        $this->status = "fail";
+    }
+    public function setData($data)
+    {
+        $this->data = ($data !== null ? $data : "");
+    }
+    public function toJSON()
+    {
+        $response = [];
+        $response['status'] = $this->status;
+        $response['data'] = $this->data;
+        return json_encode($response);
+    }
+    public function getData()
+    {
+        return $this->data;
+    }
+    public function getStatus()
+    {
+        return $this->status;
+    }
+
+}
+use Monolog\Handler\Curl\Util;
