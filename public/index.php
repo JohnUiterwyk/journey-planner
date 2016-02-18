38ac428333fba72c396f28c9c428797a42020690
John Uiterwyk <jnuiterwyk@gmail.com>
2016-02-18 18:23:06 +1100

    remove old comments from index.php

--- public/index.php
+++ public/index.php
@@ -12,19 +12,3 @@ require __DIR__ . '/../vendor/autoload.php';
 
 
 $journeyPlanner = new \JourneyPlanner\JourneyPlanner();
-
-//// Instantiate the app
-//$settings = require __DIR__ . '/../src/settings.php';
-//$app = new \Slim\App($settings);
-//
-//// Set up dependencies
-//require __DIR__ . '/../src/dependencies.php';
-//
-//// Register middleware
-//require __DIR__ . '/../src/middleware.php';
-//
-//// Register routes
-//require __DIR__ . '/../src/routes.php';
-//
-//// Run app
-//$app->run();
